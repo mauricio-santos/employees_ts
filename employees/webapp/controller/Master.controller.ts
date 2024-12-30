91d9f00d03c6d9a2d263d24374ec89aa78620206
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2024-12-30 12:33:35 +0000

    DynamicPage

--- employees/webapp/controller/Master.controller.ts
+++ employees/webapp/controller/Master.controller.ts
@@ -1,4 +1,5 @@
 import BaseController from "../helpers/BaseController";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @namespace de.santos.employees.controller
@@ -7,6 +8,12 @@ export default class Master extends BaseController {
 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
+        this.loadEmployees();
+    };
 
+    private loadEmployees(): void {
+        const model = new JSONModel();
+        model.loadData("../model/Employees.json");
+        this.setModelHelper(model, "employeeModel");
     }
 }
