99b7d9bdb8bb9067b0ad5f9605353584eb80acb7
Mauricio Vieira dos Santos <mauricio.vieiradossantos@mosolf.de>
2025-01-07 08:26:01 +0000

    Using filters with models

--- employees/webapp/controller/Master.controller.ts
+++ employees/webapp/controller/Master.controller.ts
@@ -8,6 +8,7 @@ import Filter from "sap/ui/model/Filter";
 import FilterOperator from "sap/ui/model/FilterOperator";
 import Table from "sap/m/Table";
 import ListBinding from "sap/ui/model/ListBinding";
+import Model from "sap/ui/model/Model";
 
 /**
  * @namespace de.santos.employees.controller
@@ -17,19 +18,30 @@ export default class Master extends BaseController {
     public onInit(): void {
         this.loadEmployees();
         this.loadCountries();
+        this.loadFilters();
     };
 
     private loadEmployees(): void {
-        const model = new JSONModel();
+        const model = new JSONModel() as JSONModel;
         model.loadData("../model/Employees.json");
         this.setModelHelper(model, "employeesModel");
     };
 
     private loadCountries(): void {
-        const model = new JSONModel();
+        const model = new JSONModel() as JSONModel;
         model.loadData("../model/Countries.json");
         this.setModelHelper(model, "countriesModel");
     };
+
+    private loadFilters(): void {
+        const data: {Employee: string, Country: string} = {
+            Employee: "",
+            Country: ""
+        } ;
+        const model = new JSONModel(data) as JSONModel;
+        this.setModelHelper(model, "filtersModel");
+        
+    }
 
     public onFilterBarGoSearch(event: FilterBar$SearchEvent): void {
         const controls = event.getParameter("selectionSet") as Control[];
@@ -71,6 +83,36 @@ export default class Master extends BaseController {
         input.setValue("");
         comboBox.setSelectedKey("");
         this.onFilterBarGoSearch(event);
-
     };
+
+    public onFilterGeneric(): void {
+        const model = this.getModelHelper("filtersModel") as Model;
+        const employee = model.getProperty("/Employee");
+        const country = model.getProperty("/Country");
+
+        const filters = [];
+
+        if (employee) {
+            filters.push(new Filter({
+                filters: [
+                    new Filter("EmployeeID", FilterOperator.EQ, employee),
+                    new Filter({
+                        filters: [
+                            new Filter("FirstName", FilterOperator.Contains, employee),
+                            new Filter("LastName", FilterOperator.Contains, employee)
+                        ],
+                        and: false
+                    })
+                ],
+                and: false
+            }));
+        }
+
+        country && filters.push(new Filter("Country", FilterOperator.EQ, country))
+
+        const table = this.byId("idEmployeesTable") as Table;
+        const binding = table.getBinding("items") as ListBinding;
+        binding.filter(filters); 
+        
+    }
 }
